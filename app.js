1d819c75aafec8bf8f867cd03a798484d9f81ecb
med amine <aminelife86@gmail.com>
2022-10-02 18:20:49 +0200

    save five

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 const port = process.env.PORT || 3000
 const routerPokemons = require("./src/routes/pokemons.routes")
 const routerLogin = require("./src/routes/login.routes")
