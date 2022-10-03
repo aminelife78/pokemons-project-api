29c97363282ab45acdb8fb268dd2a1b48859719d
med amine <aminelife86@gmail.com>
2022-10-03 23:30:55 +0200

    save 39

--- src/routes/pokemons.routes.js
+++ src/routes/pokemons.routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const {pokemons,pokemon,addPokemon,updatePokemon,deletePokemon}  = require("../controllers/pokemons.controllers")
 const auth = require("../auth/auth")
 
-router.get("/pokemons",auth,pokemons)
+router.get("/pokemons",pokemons)
 router.get("/pokemons/:id",auth,pokemon)
 
 router.post("/pokemons",auth,addPokemon)
