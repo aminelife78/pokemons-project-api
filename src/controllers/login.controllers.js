146df51461e7d4579c7457ea635106714a930934
med amine <aminelife86@gmail.com>
2022-10-03 11:06:26 +0200

    save 22

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -1,5 +1,5 @@
 const {User} = require("../db/sequelize")
-// const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const privateKey = require("../auth/private_key")
 
